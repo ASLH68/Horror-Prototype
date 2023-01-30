027aae9510b153bdab352134d754d8de62b2358c
Charlie Polonus <capolonus@gmail.com>
2023-01-29 23:13:01 -0600

    "Final" push

--- Assets/Scripts/EnemyScript/EnemyMovement.cs
+++ Assets/Scripts/EnemyScript/EnemyMovement.cs
@@ -60,7 +60,7 @@ public class EnemyMovement : MonoBehaviour
     {
         if (_normalMovement)
         {
-            if (_lighter.TurnedOn)
+            if (_lighter.TurnedOn && _lighter.FuelAmount > 0.1f)
             {
                 if (_chargeCoroutine != null)
                 {
